c8e31798eb4a3e62dda4dd265f24c68c5f15920f
Stefan Bielak <sbielak20@gmail.com>
2023-08-04 18:52:22 +0200

    poprawki mentora

--- js/task-02.js
+++ js/task-02.js
@@ -1,15 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatoes",
+  "Herbs",
+  "Condiments",
 ];
+
 const ingredientsList = document.getElementById("ingredients");
+const listItems = [];
+
 ingredients.forEach((ingredient) => {
-  const listItem = document.createElement("li");
-  listItem.textContent = ingredient;
-  listItem.classList.add("item");
-  ingredientsList.appendChild(listItem);
+  const li = document.createElement("li");
+  li.textContent = ingredient;
+  li.classList.add("item");
+  listItems.push(li);
 });
+
+ingredientsList.append(...listItems);
